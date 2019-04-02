856cf3f4fb890872f6bfb4e16e394425656ec167
Jason Harrison <jason@eiodiagnostics.com>
2019-04-02 16:44:18 -0700

    copy from wifi-connect/ui

--- web-app/static/js/index.js
+++ web-app/static/js/index.js
@@ -1,23 +1,40 @@
 $(function(){
-	$.get(wifiConnectEndpoint() + '/networks', function(data){
+	var networks = undefined;
+
+	function showHideEnterpriseSettings() {
+		var security = $(this).find(':selected').attr('data-security');
+		if(security === 'enterprise') {
+			$('#identity-group').show();
+		} else {
+			$('#identity-group').hide();
+		}
+	}
+
+	$('#ssid-select').change(showHideEnterpriseSettings);
+
+	$.get(wifiConnectEndpoint() + "/networks", function(data){
 		if(data.length === 0){
 			$('.before-submit').hide();
 			$('#no-networks-message').removeClass('hidden');
 		} else {
-			$('#debug').text(data);
-			$.each(JSON.parse(data), function(i, val){
-				$("#ssid-select").append($('<option>').attr('val', val).text(JSON.stringify(val));
+			networks = JSON.parse(data);
+			$.each(networks, function(i, val){
+				$('#ssid-select').append(
+					$('<option>')
+						.text(val.ssid)
+						.attr('val', val.ssid)
+						.attr('data-security', val.security)
+				);
 			});
+
+			jQuery.proxy(showHideEnterpriseSettings, $('#ssid-select'))();
 		}
 	});
 
 	$('#connect-form').submit(function(ev){
-		let serialized = $('#connect-form').serialize();
-		$.post(wifiConnectEndpoint() + '/connect', serialized, function(data){
-			$.post('/custom', serialized, function(data){
-				$('.before-submit').hide();
-				$('#submit-message').removeClass('hidden');
-			});
+		$.post(wifiConnectEndpoint() + '/connect', $('#connect-form').serialize(), function(data){
+			$('.before-submit').hide();
+			$('#submit-message').removeClass('hidden');
 		});
 		ev.preventDefault();
 	});
