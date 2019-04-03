3141a1f54ca85378fd908c7c8b7583f40c392a17
Jason Harrison <jason@eiodiagnostics.com>
2019-04-03 12:32:41 -0700

    refactoring

--- wifi-connect/scripts/start.sh
+++ wifi-connect/scripts/start.sh
@@ -6,50 +6,54 @@ readonly LOG_LOCATION=/eio-data/log/runCommand-${RESIN_SERVICE_NAME}.log
 exec > >(tee -a -i $LOG_LOCATION)
 exec 2>&1
 source util.bash
+echolog "$BASH_SOURCE started"
 
 declare haveInternetAccess=1
 declare haveWifiAccess=1
 
-echolog "wifi-connect-api started"
-nmcli connection show | echolog
-#nmcli c up edlab
 
-# TODO compare nmcl connection show against scan output
+# to connect to edlab:
+# nmcli c up edlab
 
+# TODO compare nmcli connection show against scan output
+
+main() {
+    # check for active WiFi Connection regularly 
+    while true; do
+        nmcli connection show | echolog
+
+        checkForWifi
+        if [[ $haveInternetAccess -eq "yes" && $haveWifiAccess -eq "yes" ]]; then
+            echolog 'Skipping WiFi Connect'
+            # wait 10 seconds before checking again for internet connectivity
+            sleep 10
+        else
+            echolog 'Starting WiFi Connect'
+            # Start wifi-connect on a separate port
+            ./wifi-connect \
+                --portal-ssid "EIO Camera ${RESIN_DEVICE_NAME_AT_INIT}" \
+                --portal-listening-port 45454
+            
+            wifiConnectExit=$?
+            echolog "wifi-connect exited with code $wifiConnectExit"
+        fi
+    done
+}
 
 checkForWifi() {
     wget "http://clients3.google.com/generate_204?" -O /dev/null 2>&1 | grep "204 No Content" > /dev/null
     haveInternetAccess=$?
-    echolog "haveInternetAccess=$haveInternetAccess"
+    if [[ $haveInternetAccess -eq 0 ]]; then 
+        haveInternetAccess="yes"
+    fi
+    echolog "haveInternetAccess: $haveInternetAccess"
 
     iwgetid --raw | echolog
-    echolog "haveWifiAcces=$haveWifiAccess"
     haveWifiAccess=$?
-    if [[ $haveInternetAccess -eq 0 && $haveWifiAccess -eq 0 ]]; then
-        echolog "have wifi internet access"
-        (( success++ ))
+    if [[ $haveWifiAccess -eq 0 ]]; then
+        haveWifiAccess="yes"
     fi
+    echolog "haveWifiAccess: $haveWifiAccess"
 }
 
-# check for active WiFi Connection regularly 
-while true; do
-    nmcli connection show | echolog
-
-    checkForWifi
-    if [[ $haveInternetAccess -eq 0 && $haveWifiAccess -eq 0 ]]; then
-        echolog 'Skipping WiFi Connect'
-        # wait 10 seconds before checking again for internet connectivity
-        sleep 10
-    else
-        echolog 'Starting WiFi Connect'
-        # Start wifi-connect on a separate port
-        ./wifi-connect \
-            --portal-ssid "EIO Camera ${RESIN_DEVICE_NAME_AT_INIT}" \
-            --portal-listening-port 45454
-        
-        wifiConnectExit=$?
-
-        echolog "wifi-connect exited with code $wifiConnectExit"
-    fi
-
-done
+main
